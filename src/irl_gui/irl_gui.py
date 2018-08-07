d96efd6c52e170cc83455a8916b16719c2e228a0
Laura Petrich <laura.petrich@gmail.com>
2018-08-07 16:37:21 -0600

    first commit

---
+++ src/irl_gui/irl_gui.py
@@ -0,0 +1,147 @@
+import rospy, roslib, re, sys
+sys.path.insert(0,'../')
+import numpy as np
+import base64
+from qt_gui.plugin import Plugin
+from python_qt_binding.QtCore import Qt, QTimer
+from rqt_gui.main import Main
+from std_msgs.msg import String
+from python_qt_binding.QtCore import *
+from python_qt_binding.QtGui import *
+from python_qt_binding.QtWidgets import *
+from io import BytesIO
+from PIL import Image, ImageDraw, ImageChops
+
+class IRLGuiWidget(QWidget):
+#############################################################################################################
+# INITIALIZATION METHODS
+#############################################################################################################
+    def __init__(self):
+        super(IRLGuiWidget, self).__init__()
+        self.setWindowTitle("IRL Widget")
+        # setup subscribers and publishers
+        rospy.Subscriber("/im1", String, self.callback)
+        rospy.Subscriber("/im2", String, self.callback2)
+        rospy.Subscriber("/im3", String, self.callback3)
+        rospy.Subscriber("/im_info", String, self.callback4)
+
+        self.main_layout = self.menu_layout()
+        self.setLayout(self.main_layout)
+
+#############################################################################################################
+# WIDGET 
+#############################################################################################################
+    def button1_function(self):
+        pass
+
+    def button2_function(self):
+        pass
+
+
+    def menu_layout(self):
+        l = QVBoxLayout()
+        l_view = QHBoxLayout()
+        l_view2 = QHBoxLayout()
+        button_layout = QHBoxLayout()
+
+        title = QLabel()
+        title.setText("IRL")
+        title.setWordWrap(True)
+        title.setStyleSheet("font-size: 32px")
+        title.setAlignment(Qt.AlignCenter)
+        title.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
+
+
+        button1 = QPushButton("Set 0")
+        button1.clicked.connect(self.button1_function)
+        button1.setStyleSheet("background-color: rgba(16, 123, 227, 90%); selection-background-color: rgba(16, 123, 227, 80%); font-size: 32px")
+        button1.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
+        button2 = QPushButton("Get dRt")
+        button2.clicked.connect(self.button2_function)
+        button2.setStyleSheet("background-color: rgba(16, 123, 227, 70%); selection-background-color: rgba(16, 123, 227, 60%); font-size: 32px")
+        button2.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
+        self._view = QLabel()
+        self._view.setAlignment(Qt.AlignCenter)
+        self._view.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        self._view2 = QLabel()
+        self._view2.setAlignment(Qt.AlignCenter)
+        self._view2.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        self._view3 = QLabel()
+        self._view3.setAlignment(Qt.AlignCenter)
+        self._view3.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        self._view4 = QLabel()
+        self._view4.setText("test")
+        self._view4.setWordWrap(True)
+        self._view4.setStyleSheet("font-size: 32px")
+        self._view4.setAlignment(Qt.AlignCenter)
+        self._view4.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+
+        button_layout.addWidget(button1)
+        button_layout.addWidget(button2)
+        l_view.addWidget(self._view)
+        l_view.addWidget(self._view2)
+        l_view2.addWidget(self._view3)
+        l_view2.addWidget(self._view4)
+
+        l.addWidget(title)
+        l.addLayout(l_view)
+        l.addLayout(l_view2)
+        l.addLayout(button_layout)
+        return l
+
+#############################################################################################################
+# CALLBACK METHODS
+#############################################################################################################
+    def callback(self, data):
+        s = data.data
+        p = self.convert_img(s)
+        self._view.setPixmap(p)
+
+    def callback2(self, data):
+        s = data.data
+        p = self.convert_img(s)
+        self._view2.setPixmap(p)
+
+    def callback3(self, data):
+        s = data.data
+        p = self.convert_img(s)
+        self._view3.setPixmap(p)
+
+    def callback4(self, data):
+    	msg = data.data
+    	self._view4.setText("whoop whoop " + msg)
+
+    def convert_img(self, data):
+        img = Image.open(BytesIO(base64.b64decode(data)))
+        frame = np.asarray(img)
+        qimg = QImage(frame, frame.shape[1], frame.shape[0], QImage.Format_Grayscale8)
+        pixmap = QPixmap.fromImage(qimg)
+        return pixmap
+
+#############################################################################################################
+# EVENT METHODS
+#############################################################################################################
+    def keyPressEvent(self, QKeyEvent):
+        key = QKeyEvent.key()
+        if int(key) == 82: # r
+            self.reset()
+        elif int(key) == 68: # d
+            self.debug()
+        elif int(key) == 81: # q
+            sys.exit()
+        else:
+            print("Unknown key option: " + key)
+
+
+class IRLGuiPlugin(Plugin):
+    def __init__(self, context):
+        super(IRLGuiPlugin, self).__init__(context)
+        if context.serial_number() > 1:
+            raise RuntimeError("You may not run more than one instance of visual_servoing_gui.")
+        self.setObjectName("Visual Servoing Plugin")
+        self._widget = IRLGuiWidget()
+        context.add_widget(self._widget)
+
+if __name__ == '__main__':
+    main = Main()
+    sys.exit(main.main(sys.argv, standalone='irl_gui.irl_gui_plugin.IRLGuiPlugin'))
